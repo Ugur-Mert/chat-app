30c41663d4b8006cd98dff5dcf347d22f43f84f9
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-07-14 18:44:57 +0300

    navbar logout

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import HomePage from "./components/pages/HomePage";
 import { Register } from "./components/pages/Register";
 import { Toaster } from "react-hot-toast";
 import { Welcome } from "./components/pages/Welcome";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 function App() {
   return (
     <div>
+      <ResponsiveAppBar />
       <Toaster position="top-right" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<HomePage />} />
